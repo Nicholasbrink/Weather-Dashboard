b298c8d3929d3d215343ef125b2f846debb7147c
Nicholas Brink <nicbrink@gmail.com>
2023-01-29 23:36:41 +0000

    updated javascript code and css styling

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // // Populate history list from local storage when page loads
-// const history = JSON.parse(localStorage.getItem("history")) || [];
-const history = document.querySelector("#history");
+const history = JSON.parse(localStorage.getItem("history"));
+const historySection = document.querySelector("#history");
 //Open Weather APIKey
 const apiKey = "218a901e53733b9ac26b20f0818fba33"; //weather API Key
 
@@ -11,15 +11,17 @@ const searchBtn = document.querySelector("#search-button");
 // Add the history to local storage
 searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
+
   const cityName = userInput.value;
   // Save search term as key-value pair in local storage
+  // You can store the value in localStorage with the key value pair. It has two methods setItem(key, value) to store the value in the storage object and getItem(key) to retrieve the value from the storage object. document. getElementById("result").
   localStorage.setItem(cityName, cityName);
 
   // Create button for search term
-  var button = document.createElement("button");
+  const button = document.createElement("button");
   button.className = "search-button";
   button.innerHTML = cityName;
-  history.appendChild(button);
+  historySection.appendChild(button);
 
   const queryURL =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -32,6 +34,8 @@ searchBtn.addEventListener("click", function (event) {
     const lat = georesponse.data[0].lat;
     const lon = georesponse.data[0].lon;
 
+    console.log(georesponse);
+
     const weatherQueryURL =
       "https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=" +
       lat +
@@ -39,37 +43,106 @@ searchBtn.addEventListener("click", function (event) {
       lon +
       "&appid=" +
       apiKey;
-    console.log(georesponse);
-    //   axios.get(weatherQueryURL).then(function (weatherResponse) {
-    //     const cityData = response.data;
-    //   });
+
+    axios.get(weatherQueryURL).then(function (response) {
+      const cityData = response.data;
+
+      //Call 5 day weather forecast API after we have city lat and lon value
+
+      //Set variable called weatherList to search through the entire list:Array (40 items)
+      const weatherList = cityData.list;
+      var j = 0;
+
+      // Each object in the above array is every 3 hours so pick out one in very 8 to have 1 per day for 5 days
+      for (let i = 0; i < weatherList.length; i += 8) {
+        const weather = weatherList[i];
+        console.log(response);
+
+        // Get todays date
+        const today = weatherList[0];
+        const todayTimestamp = today.today;
+
+        // getting the data only (without time) using moment.js to convert unix
+        const timestamp = weatherList[i].dt;
+        const dateConverted = moment.unix(timestamp).format("dddd DD/MM/YYYY");
+
+        //adding the location information, with city and country
+        const locationDate =
+          response.data.city.Name +
+          ", " +
+          response.data.city.country +
+          " on " +
+          dateConverted +
+          " is:";
+
+        // the icon for the current weather
+
+        const icon =
+          "https://openweathermap.org/img/wn/" +
+          weatherList[i].weather[0].icon +
+          "@2x.png";
+
+        // the temperature
+        const temp = " Temp: " + weatherList[i].main.temp.toFixed(1) + " °C";
+        // windspeed
+        const wind = " Wind: " + weatherList[i].wind.speed + " KPH";
+
+        // the humidity
+        const humidity = " Humidity: " + weatherList[i].main.humidity + "%";
+
+        //console logging the whole report to check
+        //const weatherResult = locationDate + icon + temp + wind + humidity;
+        //console.log(weatherResult);
+
+        if (j == 0) {
+          // document.querySelector("today").innerHTML = "";
+          document.querySelector(".location").innerHTML =
+            "The weather in " + cityName + " on " + dateConverted + " is ";
+          document.querySelector(".temp").innerHTML = temp;
+          document.querySelector(".wind").innerHTML = wind;
+          document.querySelector(".humidity").innerHTML = humidity;
+          document.querySelector(".icon").src = icon;
+        } else {
+          const forecast = document.createElement("div");
+          forecast.classList.add("card", "col-md-2", "mb-3");
+          forecast.innerHTML =
+            "<h5>" +
+            dateConverted +
+            "</h5>" +
+            "<img class='icon' src='" +
+            icon +
+            "'>" +
+            "<p class='card-text temp'>" +
+            temp +
+            "</p>" +
+            "<p class='card-text wind'>" +
+            wind +
+            "</p>" +
+            "<p class='card-text humidity'>" +
+            humidity +
+            "</p>";
+          document.querySelector("#fiveDayForecast").appendChild(forecast);
+        }
+        j++;
+        if (j > 5) break;
+      }
+
+      // displays last city searched for or default value
+    });
   });
 });
-// Call 5 day weather forecast API after we have city lat and lon value
 
-// Icon URL http://openweathermap.org/img/w/" + iconcode + ".png"
+window.addEventListener("load", function (event) {
+  const keys = Object.keys(localStorage);
 
-// Put the response on the HTML page
-//     const weatherList = weatherResponse.list;
+  // Loop through keys
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
 
-//     // 5 days forecast
-//     for (let i = 1; i < weatherList.length; i += 8) {
-//       const weather = weatherList[i];
-//       console.log(weather);
-//     }
-
-//     const today = weatherList[0];
-//     const todayTimestamp = today.today;
-//     console.log(todayTimestamp);
-
-//     const temp = document.createElement("p");
-//     (weatherList[i].main.temp - 273.15).toFixed(1) + " °C";
-//     //   var wind = document.createElement("p");
-//     //   var humidity = document.createElement("p");
-//   });
-
-//   // history.pushState(userInput);
-//   // localStorage.setItem("temp");
-
-//   // localStorage.setItem("history", JSON.stringify(history))
-// });
+    // Create button for key
+    var button = document.createElement("button");
+    button.className = "search-button";
+    button.innerHTML = key;
+    historySection.appendChild(button);
+  }
+});
